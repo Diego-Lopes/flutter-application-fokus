424472de0e3f4ed2e7c73583b313874840053f82
Diego-Lopes <diegolopes2009@hotmail.com>
2026-01-09 21:28:04 -0400

    📦lib: added provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,30 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:fokus/app/shared/enums/timer_type.dart';
 import 'package:fokus/app/view/pages/home_page.dart';
 import 'package:fokus/app/view/pages/timer_page.dart';
+import 'package:fokus/app/view_model/timer_view_model.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/home': (context) => HomePage(),
-        '/timer': (context) => TimerPage(
-          timerType:
-              // definindo parâmetros
-              // vindo de settings.arguments que definimos na page home, caso não tenha usamos default timertype.focus
-              (ModalRoute.of(context)?.settings.arguments as TimerType?) ??
-              TimerType.focus,
+    return ChangeNotifierProvider(
+      create: (_) => TimerViewModel(),
+      child: MaterialApp(
+        routes: {
+          '/home': (context) => HomePage(),
+          '/timer': (context) => TimerPage(
+            timerType:
+                // definindo parâmetros
+                // vindo de settings.arguments que definimos na page home, caso não tenha usamos default timertype.focus
+                (ModalRoute.of(context)?.settings.arguments as TimerType?) ??
+                TimerType.focus,
+          ),
+        },
+        title: 'Fokus',
+        theme: ThemeData(
+          useMaterial3: true,
+          textTheme: GoogleFonts.unboundedTextTheme(),
         ),
-      },
-      title: 'Fokus',
-      theme: ThemeData(
-        useMaterial3: true,
-        textTheme: GoogleFonts.unboundedTextTheme(),
+        initialRoute: '/home',
       ),
-      initialRoute: '/home',
     );
   }
 }
