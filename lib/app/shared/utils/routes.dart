e6086ee69692ac5f1866acf0e9b59be5694b26bd
Diego-Lopes <diegolopes2009@hotmail.com>
2026-02-09 22:07:50 -0400

    ♻️refactor: create file of routes

---
+++ lib/app/shared/utils/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fokus/app/shared/enums/timer_type.dart';
+import 'package:fokus/app/view/pages/home_page.dart';
+import 'package:fokus/app/view/pages/timer_page.dart';
+
+Map<String, Widget Function(BuildContext)> get routes =>
+    <String, WidgetBuilder>{
+      '/home': (context) => HomePage(),
+      '/timer': (context) => TimerPage(
+        timerType:
+            (ModalRoute.of(context)?.settings.arguments as TimerType?) ??
+            TimerType.focus,
+      ),
+    };
